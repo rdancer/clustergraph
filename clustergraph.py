f3a5d05ff5fcc2c48e6739db42aff7772cf79e9e
Jan Minář <rdancer@rdancer.org>
2010-09-15 00:57:13 +0100

    License change: GPLv2 -> GPLv3

--- clustergraph.py
+++ clustergraph.py
@@ -1,26 +1,28 @@
 # clustergraph.py -- graph clustered data points
 #encoding:UTF-8
 
-# Copyright © 2010 Jan Minář <rdancer@rdancer.org>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2 (two),
-# as published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+Copyright © 2010 Jan Minář <rdancer@rdancer.org>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 (three),
+as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
 
 #
 # Usage
 #
 # Copy the “clustergraph.py” file into the directory alongside the python
-# script.  Then, use the ‘import’ stanza, and then simply call
+# script.  Use the ‘import’ stanza, and then simply call
 # clustergraph.drawGraph():
 #
 #   import clustergraph 
