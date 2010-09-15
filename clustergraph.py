f561888c4a2ab59f3c60c86bf9c223eb5b85f38e
Jan Minář <rdancer@rdancer.org>
2010-09-15 02:07:45 +0100

    Graph single cluster

--- clustergraph.py
+++ clustergraph.py
@@ -3,6 +3,7 @@
 
 """
 Copyright © 2010 Jan Minář <rdancer@rdancer.org>
+Copyright Gerald Kaszuba 2008
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License version 3 (three),
@@ -18,6 +19,9 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
+__version__ = "0.0.0"
+__author__ = "Jan Minář <rdancer@rdancer.org>"
+
 #
 # Usage
 #
@@ -29,6 +33,40 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #   clustergraph.drawGraph([[1.0, 1.1], [1.2, 1.3, 1.4], [1.5]])
 #
 
+import os
+import sys
+import math
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(ROOT, '..'))
+
+from pygooglechart import ScatterChart
+
+#import settings
+#import helper
+
 
 def drawGraph(clusters):
     """Draw a Cartesian graph on screen; each cluster has a different colour."""
+
+    width = 250
+    height = width
+    padding = 30
+    xlist = []
+    ylist = []
+    coords = []
+
+    for cluster in clusters:
+	for point in cluster.points:
+	    for i in xrange(point.n):
+		coords[i].append(point.coords[i])
+
+    xlist = coords[0]
+    ylist = coords[0]
+
+    chart = ScatterChart(width, height, 
+                         x_range=(min(xlist) - padding, max(xlist) + padding),
+                         y_range=(min(ylist) - padding, max(ylist) + padding))
+    chart.add_data(xlist)
+    chart.add_data(ylist)
+    chart.download('clusters.png')
