2c49aed253cddd0cf13f7fb40cc7a4b69e6cf20d
Jan Minář <rdancer@rdancer.org>
2010-09-15 05:32:30 +0100

    Colours!

--- clustergraph.py
+++ clustergraph.py
@@ -19,7 +19,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 __author__ = "Jan Minář <rdancer@rdancer.org>"
 
 #
